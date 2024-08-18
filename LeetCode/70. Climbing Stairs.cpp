bd12dd5e035c087d619f43a8432a47afb3fbe592
mutianzh <44654578+mutianzh@users.noreply.github.com>
2024-08-18 10:26:19 -0700

    Update 70. Climbing Stairs.cpp

--- LeetCode/70. Climbing Stairs.cpp
+++ LeetCode/70. Climbing Stairs.cpp
@@ -1,4 +1,4 @@
-// Sol 1
+// Sol 1: recursion with memorization
 class Solution {
 public:
     int climbStairs(int n, std::vector<int>& memo) {
@@ -17,7 +17,7 @@ public:
     }
 };
 
-//Sol 2
+//Sol 2: table only
 class Solution {
 public:
     int climbStairs(int n) {
@@ -29,5 +29,26 @@ public:
             }
         }
         return memo[n];
+    }
+};
+
+
+// Sol 3: O(1) space
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 1) {return 1;}
+        else {
+            int prev_prev = 1;
+            int prev = 1;
+            int cur = 0;
+            for (int i=2; i <= n; i++){
+                cur = prev_prev + prev;
+                prev_prev = prev;
+                prev = cur;
+            }
+            return cur;
+        }
+
     }
 };
